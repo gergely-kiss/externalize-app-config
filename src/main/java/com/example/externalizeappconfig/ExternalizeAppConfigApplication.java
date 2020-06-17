973124a52b54f4003de1bdf0a4799f3a9b95d60b
gergely <kiss-gergely@outlook.com>
2020-06-17 09:31:06 +0100

    first commit

---
+++ src/main/java/com/example/externalizeappconfig/ExternalizeAppConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalizeappconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalizeAppConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalizeAppConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/externalizeappconfig/ExternalizeAppConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalizeappconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalizeAppConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
